9b2675577905140f08f6f7075147a8c315775a0e
Izabela <izabelaszeremeta@gmail.com>
2023-02-24 10:53:18 -0600

    update ratings

--- controllers/ratings.js
+++ controllers/ratings.js
@@ -3,14 +3,32 @@ const { Rating, Profile } = require('../models')
 async function castRating(req, res) {
   try {
     req.body.ratingId = req.user.profile.id
-    
-    const rating = await Rating.create(req.body)
-    res.status(200).json(rating)
+
+    const prevRating = await Rating.findOne({
+      where: {
+        ratingId: req.body.ratingId,
+        profileId: req.body.profileId,
+      }
+    })
+
+    if (prevRating) {
+      prevRating.value = req.body.value
+      await prevRating.save()
+    } else {
+      await Rating.create(req.body)
+    }
+
+    const profile = await Profile.findByPk(
+      req.body.profileId,
+      { include: [{ model: Rating, as: "ratingsReceived" }] }
+    )
+
+    res.status(200).json(profile)
   } catch (error) {
     res.status(500).json({ err: error })
   }
 }
 
 module.exports = { 
-  castRating
+  castRating,
 }
